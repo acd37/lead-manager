f82eb9059cbcc542c9771f8bb93be4850baf7277
Alec Down <alecdown@gmail.com>
2019-10-18 18:46:24 -0500

    .

--- leadmanager/urls.py
+++ leadmanager/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('', include('leads.urls')),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
+
 ]
