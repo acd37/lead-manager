92513b444a791d358ccfd754746ee547d4473ae9
Alec Down <alecdown@gmail.com>
2019-10-18 17:42:25 -0500

    .

--- leadmanager/urls.py
+++ leadmanager/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('client.urls')),
     path('', include('leads.urls')),
     path('', include('accounts.urls'))
 ]
