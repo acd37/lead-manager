498d03f40ad86f0476e49fa8d5245676ecef188f
Alec Down <alecdown@gmail.com>
2019-10-19 08:47:41 -0500

    .

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
 }
 
--- leadmanager/urls.py
+++ leadmanager/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', include('leads.urls')),
     path('', include('accounts.urls')),
-re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
